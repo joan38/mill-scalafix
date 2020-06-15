72d5ca07cb0abc8ca09c3b0d0c83f78b418e7d1a
Joan Goyeau <joan@goyeau.com>
2020-06-15 12:52:38 -0700

    StyleModule to combine Scalafmt and Scalafix together

---
+++ mill-scalafix/src/com/goyeau/mill/scalafix/StyleModule.scala
@@ -0,0 +1,22 @@
+package com.goyeau.mill.scalafix
+
+import mill.T
+import mill.define.Command
+import mill.scalalib.scalafmt.ScalafmtModule
+
+/**
+  * Combine Scalafmt and Scalafix together
+  */
+trait StyleModule extends ScalafmtModule with ScalafixModule {
+  def style(): Command[Unit] =
+    T.command {
+      reformat()
+      fix()
+    }
+
+  def checkStyle(): Command[Unit] =
+    T.command {
+      checkFormat()
+      fix("--check")
+    }
+}
