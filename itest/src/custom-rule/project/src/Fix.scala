6034b39276f3eddfc8490383668b9301fa9be946
Scala Steward <me@scala-steward.org>
2021-08-13 20:24:31 +0200

    Reformat with scalafmt 3.0.0-RC7

--- itest/src/custom-rule/project/src/Fix.scala
+++ itest/src/custom-rule/project/src/Fix.scala
@@ -2,12 +2,12 @@ import scala.language.postfixOps
 object Tuple2ZippedSrc213 {
   def zipped(xs: List[Int], ys: List[Int]): Unit = {
     (xs, ys).zipped
-    (xs,ys).zipped
+    (xs, ys).zipped
     ((xs, ys) zipped)
-    (((xs)    ,    (ys)).zipped)
-    (xs,
-      ys).zipped
-    /* a */(/* b */ xs /* c */, /* d */ ys /* e */)/* f */./* g */zipped/* h */
+    ((xs, ys).zipped)
+    (xs, ys).zipped
+    /* a */
+    ( /* b */ xs /* c */, /* d */ ys /* e */ ) /* f */ . /* g */ zipped /* h */
     (coll(1), coll(2)).zipped
     (List(1, 2, 3), Array(1)).zipped
   }
