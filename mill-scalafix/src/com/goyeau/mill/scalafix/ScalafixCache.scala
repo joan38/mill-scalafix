f959deb796ffbeae31fdc0380ac54f5d59491f41
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-09-30 17:15:23 +0200

    Use ConcurrentHashMap for better thread safety

--- mill-scalafix/src/com/goyeau/mill/scalafix/ScalafixCache.scala
+++ mill-scalafix/src/com/goyeau/mill/scalafix/ScalafixCache.scala
@@ -4,21 +4,21 @@ import com.goyeau.mill.scalafix.CoursierUtils
 import coursier.core.Repository
 import scalafix.interfaces.Scalafix
 
-import scala.collection.mutable
-import scala.ref.SoftReference
+import java.util.concurrent.ConcurrentHashMap
 import scala.jdk.CollectionConverters._
+import scala.ref.SoftReference
 
 private[scalafix] object ScalafixCache {
 
-  private val cache = mutable.Map.empty[(String, Seq[Repository]), SoftReference[Scalafix]]
+  private val cache = new ConcurrentHashMap[(String, Seq[Repository]), SoftReference[Scalafix]]
 
   def getOrElseCreate(scalaVersion: String, repositories: Seq[Repository]) =
     cache.get((scalaVersion, repositories)) match {
-      case Some(SoftReference(value)) => value
+      case SoftReference(value) => value
       case _ =>
         val newResult =
           Scalafix.fetchAndClassloadInstance(scalaVersion, repositories.map(CoursierUtils.toApiRepository).asJava)
-        cache.update((scalaVersion, repositories), SoftReference(newResult))
+        cache.put((scalaVersion, repositories), SoftReference(newResult))
         newResult
     }
 }
