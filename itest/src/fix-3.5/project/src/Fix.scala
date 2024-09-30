5cde0aed9209ba4f4d81bc19f2af65744d16811f
Scala Steward <scala_steward@virtuslab.com>
2024-09-30 16:13:08 +0000

    Reformat with scalafmt 3.8.3
    Executed command: scalafmt --non-interactive

--- itest/src/fix-3.5/project/src/Fix.scala
+++ itest/src/fix-3.5/project/src/Fix.scala
@@ -1,5 +1,5 @@
 object Fix {
-  // use a 3.5.x-only feature to fail if a Scala 3 LTS compiler is used 
-  // https://www.scala-lang.org/blog/2024/08/22/scala-3.5.0-released.html#support-for-binary-integer-literals 
+  // use a 3.5.x-only feature to fail if a Scala 3 LTS compiler is used
+  // https://www.scala-lang.org/blog/2024/08/22/scala-3.5.0-released.html#support-for-binary-integer-literals
   def myComplexMethod = 1.to(0B1010).map(i => i -> i.toString).toMap
 }
