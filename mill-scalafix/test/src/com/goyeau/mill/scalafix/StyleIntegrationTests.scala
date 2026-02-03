7a3906cfb6a068d432daf482d474aa878f5e4673
Joan Goyeau <joan@goyeau.com>
2026-02-03 15:45:52 -0500

    Upgrade munit to 1.2.2 and Enable fmt on test

--- mill-scalafix/test/src/com/goyeau/mill/scalafix/StyleIntegrationTests.scala
+++ mill-scalafix/test/src/com/goyeau/mill/scalafix/StyleIntegrationTests.scala
@@ -1,9 +1,10 @@
 package com.goyeau.mill.scalafix
 
 import munit.FunSuite
+
 import scala.concurrent.duration.*
 
-class StyleIntegrationTests extends FunSuite {
+class StyleIntegrationTests extends FunSuite:
   override val munitTimeout: Duration = 2.minute
 
   test("style should fix and format the code") {
@@ -12,12 +13,12 @@ class StyleIntegrationTests extends FunSuite {
     assert(result.isSuccess, result.err)
 
     val fixedScala = os.read(tester.workspacePath / "project" / "src" / "Fix.scala")
-    val expected = """
-                     |
-                     |object Fix {
-                     |  def myComplexMethod: Map[Int, String] = 1.to(10).map(i => i -> i.toString).toMap
-                     |}
-                     |""".stripMargin
+    val expected   = """
+                       |
+                       |object Fix {
+                       |  def myComplexMethod: Map[Int, String] = 1.to(10).map(i => i -> i.toString).toMap
+                       |}
+                       |""".stripMargin
     assertEquals(fixedScala, expected)
   }
 
@@ -32,4 +33,3 @@ class StyleIntegrationTests extends FunSuite {
     val result = tester.eval(Seq("project.checkStyle"))
     assert(!result.isSuccess, "Check should have failed")
   }
-}
